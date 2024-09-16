280389f59965138d3ba74a0a4cf644a37ba29f0b
IpsitMallik <ipsit.mallik@openturf.in>
2024-09-16 10:58:08 +0530

    code updated

--- src/main/java/com/narada/sdk/api_client/NaradaApiService.java
+++ src/main/java/com/narada/sdk/api_client/NaradaApiService.java
@@ -1,6 +1,5 @@
 package com.narada.sdk.api_client;
 
-
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -16,25 +15,28 @@ import jakarta.xml.ws.Service;
  * Generated source version: 4.0.5
  *
  */
-@WebServiceClient(name = "NaradaApiService",
-                  wsdlLocation = "file:/D:/OpenTurf_Technologies/Projects/NaradaApiService.wsdl",
-                  targetNamespace = "http://services.api.ycs.com")
+@WebServiceClient(name = "NaradaApiService", wsdlLocation = "classpath:NaradaApiService.wsdl", targetNamespace = "http://services.api.ycs.com")
 public class NaradaApiService extends Service {
 
     public static final URL WSDL_LOCATION;
 
     public static final QName SERVICE = new QName("http://services.api.ycs.com", "NaradaApiService");
-    public static final QName NaradaApiServiceHttpEndpoint = new QName("http://services.api.ycs.com", "NaradaApiServiceHttpEndpoint");
-    public static final QName NaradaApiServiceHttpSoap11Endpoint = new QName("http://services.api.ycs.com", "NaradaApiServiceHttpSoap11Endpoint");
-    public static final QName NaradaApiServiceHttpSoap12Endpoint = new QName("http://services.api.ycs.com", "NaradaApiServiceHttpSoap12Endpoint");
+    public static final QName NaradaApiServiceHttpEndpoint = new QName("http://services.api.ycs.com",
+            "NaradaApiServiceHttpEndpoint");
+    public static final QName NaradaApiServiceHttpSoap11Endpoint = new QName("http://services.api.ycs.com",
+            "NaradaApiServiceHttpSoap11Endpoint");
+    public static final QName NaradaApiServiceHttpSoap12Endpoint = new QName("http://services.api.ycs.com",
+            "NaradaApiServiceHttpSoap12Endpoint");
     static {
         URL url = null;
         try {
-            url = URI.create("file:/D:/OpenTurf_Technologies/Projects/NaradaApiService.wsdl").toURL();
-        } catch (MalformedURLException e) {
+            url = NaradaApiService.class.getClassLoader().getResource("NaradaApiService.wsdl");
+
+        } catch (Exception e) {
             java.util.logging.Logger.getLogger(NaradaApiService.class.getName())
-                .log(java.util.logging.Level.INFO,
-                     "Can not initialize the default wsdl from {0}", "file:/D:/OpenTurf_Technologies/Projects/NaradaApiService.wsdl");
+                    .log(java.util.logging.Level.INFO,
+                            "Can not initialize the default wsdl from {0}",
+                            "file:/D:/OpenTurf_Technologies/Projects/NaradaApiService.wsdl");
         }
         WSDL_LOCATION = url;
     }
@@ -51,25 +53,22 @@ public class NaradaApiService extends Service {
         super(WSDL_LOCATION, SERVICE);
     }
 
-    public NaradaApiService(WebServiceFeature ... features) {
+    public NaradaApiService(WebServiceFeature... features) {
         super(WSDL_LOCATION, SERVICE, features);
     }
 
-    public NaradaApiService(URL wsdlLocation, WebServiceFeature ... features) {
+    public NaradaApiService(URL wsdlLocation, WebServiceFeature... features) {
         super(wsdlLocation, SERVICE, features);
     }
 
-    public NaradaApiService(URL wsdlLocation, QName serviceName, WebServiceFeature ... features) {
+    public NaradaApiService(URL wsdlLocation, QName serviceName, WebServiceFeature... features) {
         super(wsdlLocation, serviceName, features);
     }
-
-
-
 
     /**
      *
      * @return
-     *     returns NaradaApiServicePortType
+     *         returns NaradaApiServicePortType
      */
     @WebEndpoint(name = "NaradaApiServiceHttpEndpoint")
     public NaradaApiServicePortType getNaradaApiServiceHttpEndpoint() {
@@ -79,20 +78,22 @@ public class NaradaApiService extends Service {
     /**
      *
      * @param features
-     *     A list of {@link jakarta.xml.ws.WebServiceFeature} to configure on the proxy.  Supported features not in the <code>features</code> parameter will have their default values.
+     *                 A list of {@link jakarta.xml.ws.WebServiceFeature} to
+     *                 configure on the proxy. Supported features not in the
+     *                 <code>features</code> parameter will have their default
+     *                 values.
      * @return
-     *     returns NaradaApiServicePortType
+     *         returns NaradaApiServicePortType
      */
     @WebEndpoint(name = "NaradaApiServiceHttpEndpoint")
     public NaradaApiServicePortType getNaradaApiServiceHttpEndpoint(WebServiceFeature... features) {
         return super.getPort(NaradaApiServiceHttpEndpoint, NaradaApiServicePortType.class, features);
     }
 
-
     /**
      *
      * @return
-     *     returns NaradaApiServicePortType
+     *         returns NaradaApiServicePortType
      */
     @WebEndpoint(name = "NaradaApiServiceHttpSoap11Endpoint")
     public NaradaApiServicePortType getNaradaApiServiceHttpSoap11Endpoint() {
@@ -102,20 +103,22 @@ public class NaradaApiService extends Service {
     /**
      *
      * @param features
-     *     A list of {@link jakarta.xml.ws.WebServiceFeature} to configure on the proxy.  Supported features not in the <code>features</code> parameter will have their default values.
+     *                 A list of {@link jakarta.xml.ws.WebServiceFeature} to
+     *                 configure on the proxy. Supported features not in the
+     *                 <code>features</code> parameter will have their default
+     *                 values.
      * @return
-     *     returns NaradaApiServicePortType
+     *         returns NaradaApiServicePortType
      */
     @WebEndpoint(name = "NaradaApiServiceHttpSoap11Endpoint")
     public NaradaApiServicePortType getNaradaApiServiceHttpSoap11Endpoint(WebServiceFeature... features) {
         return super.getPort(NaradaApiServiceHttpSoap11Endpoint, NaradaApiServicePortType.class, features);
     }
 
-
     /**
      *
      * @return
-     *     returns NaradaApiServicePortType
+     *         returns NaradaApiServicePortType
      */
     @WebEndpoint(name = "NaradaApiServiceHttpSoap12Endpoint")
     public NaradaApiServicePortType getNaradaApiServiceHttpSoap12Endpoint() {
@@ -125,9 +128,12 @@ public class NaradaApiService extends Service {
     /**
      *
      * @param features
-     *     A list of {@link jakarta.xml.ws.WebServiceFeature} to configure on the proxy.  Supported features not in the <code>features</code> parameter will have their default values.
+     *                 A list of {@link jakarta.xml.ws.WebServiceFeature} to
+     *                 configure on the proxy. Supported features not in the
+     *                 <code>features</code> parameter will have their default
+     *                 values.
      * @return
-     *     returns NaradaApiServicePortType
+     *         returns NaradaApiServicePortType
      */
     @WebEndpoint(name = "NaradaApiServiceHttpSoap12Endpoint")
     public NaradaApiServicePortType getNaradaApiServiceHttpSoap12Endpoint(WebServiceFeature... features) {
